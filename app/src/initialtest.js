cd688d48b041761ddaddb6bc7c69a0b94e25073d
Nick <smith.nicholas.isu@gmail.com>
2016-06-19 08:26:55 -0400

    Testing code coverage

--- app/src/initialtest.js
+++ app/src/initialtest.js
@@ -2,6 +2,32 @@ function returnTrue() {
     return true;
 }
 
+function returnTrue2() {
+    return true;
+}
+
+function returnTrue3() {
+    return true;
+}
+
+function returnTrue4() {
+    return true;
+}
+
+function returnTrue5() {
+    return true;
+}
+
+function returnTrue6() {
+    return true;
+}
+
+returnTrue2();
+returnTrue3();
+returnTrue4();
+returnTrue5();
+returnTrue6();
+
 if (typeof module !== 'undefined' && module.exports !== null) {
     exports.returnTrue = returnTrue;
 }
