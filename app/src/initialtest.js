6f2db2a49a574945c15ea292b8ec10e31d559636
Nick <smith.nicholas.isu@gmail.com>
2016-06-19 09:08:19 -0400

    Removing unneeded functions

--- app/src/initialtest.js
+++ app/src/initialtest.js
@@ -4,14 +4,6 @@ function returnTrue() {
     return true;
 }
 
-function returnTrue2() { // eslint-disable-line no-unused-vars
-    return true;
-}
-
-function returnTrue3() { // eslint-disable-line no-unused-vars
-    return true;
-}
-
 if (typeof module !== 'undefined' && module.exports !== null) {
     exports.returnTrue = returnTrue;
 }
