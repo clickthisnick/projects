fec609c4c0e92482315bb4cd9c605cba74527574
Nick <smith.nicholas.isu@gmail.com>
2016-06-22 20:58:22 -0400

    Fix

--- api/angularTaskList/paths.php
+++ api/angularTaskList/paths.php
@@ -3,9 +3,9 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 $paths = [
     "root" => $root,
-    "authentication" => $root . '/me/api/authentication.php',
-    "connection" => $root . '/me/api/angularTaskList/database/connection.php',
-    "rb" => $root . '/me/api/rb.php',
+    "authentication" => $root . '/projects/api/authentication.php',
+    "connection" => $root . '/projects/api/angularTaskList/database/connection.php',
+    "rb" => $root . '/projects/api/rb.php',
 ];
 
 require_once($paths['rb']);
